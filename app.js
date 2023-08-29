78fa76d2756dce26eeb8ff15153dfcbf83d89b70
NikoKemppainen <48498@edu.raseko.fi>
2023-08-29 22:04:11 +0300

    fixed

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-// WEB SERVER FOR ELECTRICITY USAGE PLANNING WEB PAGES AND AN API
+// WEB SERVER FOR ELECTRICITY USAGE PLANNING: WEB PAGES AND AN API
 // ==============================================================
 
 // LIBRARIES AND MODULES
-// =====================
+// ---------------------
 
-// USE EXPRESS AS WEB ENGINE
-const express = require("express");
-// Use Express handlebars as template engine
-const {engine} = require("express-handlebars");
+// Use Express as web engine
+const express = require('express');
+// Use Express Handlebars as template engine
+const {engine} = require('express-handlebars');
 
 // EXPRESS APPLICATION SETTINGS
-// ---------------------------
+// ----------------------------
 
 // Create the server
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// set folder paths: public is for assets and views for pages
-app.use(express.static("public"));
-app.set("views", "./views");
+// Set folder paths: public is for assets and views for pages
+app.use(express.static('public'));
+app.set('views', './views');
 
 // Engine settings
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 
 // URL ROUTES
-//-----------
+// ----------
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get('/', (req, res) => {
+    res.render('index')
 
 });
 
-
 // START THE LISTENER
 app.listen(PORT);
-console.log("Server started and it will listen TCP port", PORT);
+console.log('Server started and it will listen TCP port', PORT);
