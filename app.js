439eb4e544ec69c1e14423d34de03dc28b022ec2
NikoKemppainen <48498@edu.raseko.fi>
2023-09-05 00:40:36 +0300

    no kyll koodia pirusti

--- app.js
+++ app.js
@@ -27,22 +27,38 @@ app.set('view engine', 'handlebars');
 // URL ROUTES
 // ----------
 
+// TODO: add date and time as dynamic data for the homepage, is it sensible to use server for creating
+
+
+// Route to home page
 app.get('/', (req, res) => {
    
+    // Handlebars needs a key to show data on a page, json is a good way to send it
     let homePageData = {
         "price": 31.25, 
         "wind": 2,
         "temperature": 18,
     }
+
+    // Rander index.handlebars and sen dynamic data to the page
     res.render('index', homePageData)
 
 });
 
-app.get("hourly",(req) => {
+// Route to hourly data page 
+app.get("/hourly",(req, res) => {
 
-    let hourlyPageData = {
-        "hour": 13
-        "price": 31.44
+    // Data will be presented in a table. To loop all rows we need a key for table and for column data
+    let hourlyPageData = { "tableData": [
+        {"hour": 13,
+        "price": 31.44},
+        {"hour": 14,
+        "price": 32.10},
+        {"hour": 15,
+        "price": 30.50},
+        {"hour": 16,
+        "price": 29.99}
+    ]
     };
 
     res.render("hourly", hourlyPageData)
